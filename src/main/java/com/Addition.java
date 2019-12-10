f513c9fb7105bb82df93df01e24f9270c37a8715
chetanya1992@gmail.com <chetanya1992@gmail.com>
2019-12-10 13:10:58 +0530

    addition.java

---
+++ src/main/java/com/Addition.java
@@ -0,0 +1,8 @@
+package com;
+
+public class Addition {
+    public int add(int a,int b){
+        System.out.println(a+b);
+        return a+b;
+    }
+}
--- src/main/java/com/Basic.java
+++ src/main/java/com/Basic.java
@@ -5,5 +5,7 @@ public class Basic {
     public static void main (String[] args){
         System.out.println("hello from basic class");
         new Basic123().city();
+
+        System.out.println( new Addition().add(10,50));
     }
 }
