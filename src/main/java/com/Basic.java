27f9fc601dd997f19ecfec36825876c26596c491
chetanya1992@gmail.com <chetanya1992@gmail.com>
2019-12-10 12:34:05 +0530

    add untitled project

---
+++ src/main/java/com/Basic.java
@@ -0,0 +1,9 @@
+package com;
+
+public class Basic {
+
+    public static void main (String[] args){
+        System.out.println("hello from basic class");
+        new Basic123().city();
+    }
+}
---
+++ src/main/java/com/Basic123.java
@@ -0,0 +1,17 @@
+package com;
+
+public class Basic123 {
+    public void printString()
+    {
+        System.out.println("print String method");
+    }
+    public void name()
+    {
+        System.out.println("print name method");
+    }
+    public void city()
+    {
+        System.out.println("print city method");
+    }
+
+}
